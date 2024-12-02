f55ef994f35f6bd78bde92a293fe6e73bdea31da
hanisiddiquii <hanisiddiqui123@gmail.com>
2024-12-02 19:58:00 +0500

    exceptions added

---
+++ src/main/java/org/hivebyte/airlinereservationsystem/exception/AirportNotFoundException.java
@@ -0,0 +1,7 @@
+package org.hivebyte.airlinereservationsystem.exception;
+
+public class AirportNotFoundException extends Exception {
+    public AirportNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/hivebyte/airlinereservationsystem/exception/RouteAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.hivebyte.airlinereservationsystem.exception;
+
+public class RouteAlreadyExistsException extends Exception {
+    public RouteAlreadyExistsException(String message) {
+        super(message);
+    }
+}
